150f3683fdeeaf0cd3b3a4b9773c515ab800421d
Vlad <vlad.gafarov02@gmail.com>
2022-02-27 13:30:35 +0500

    search courses

--- apps/frontend-react/src/components/Layout/Nav.tsx
+++ apps/frontend-react/src/components/Layout/Nav.tsx
@@ -1,12 +1,19 @@
 import { useLogOutMutation } from '@api/generated'
 import { useUser } from '@lib/useUser'
-import { Menu } from '@mantine/core'
-import React from 'react'
+import { Menu, Modal } from '@mantine/core'
+import React, { useState } from 'react'
+import { BiSearch } from 'react-icons/bi'
 import { useQueryClient } from 'react-query'
 import { Link, useNavigate } from 'react-router-dom'
 import { Button } from 'ui'
+import Search from './Search'
 
 const Nav = () => {
+   const [isOpen, setIsOpen] = useState<boolean>(false)
+
+   const onOpen = () => setIsOpen(true)
+   const onClose = () => setIsOpen(false)
+
    const navigate = useNavigate()
    const { user } = useUser()
 
@@ -19,45 +26,56 @@ const Nav = () => {
    })
 
    return (
-      <div className="flex items-center space-x-3">
-         <Link to="/courses" className="font-proxima-medium">
-            Курсы
-         </Link>
-         <div className="space-x-3">
-            {user ? (
-               <Menu
-                  control={
-                     <h2 className="hover:underline hover:decoration-green-500">
-                        {user.name} {user.lastName}
-                     </h2>
-                  }
-                  transition="rotate-right"
-               >
-                  <Menu.Item
-                     onClick={() => navigate('/profile')}
-                     className="hover:bg-blue-100"
+      <>
+         <nav className="flex items-center space-x-3">
+            <button onClick={onOpen} className="text-xl">
+               <BiSearch />
+            </button>
+            <Link to="/courses" className="font-proxima-medium">
+               Курсы
+            </Link>
+            <div className="space-x-3">
+               {user ? (
+                  <Menu
+                     control={
+                        <h2 className="hover:underline hover:decoration-green-500">
+                           {user.name} {user.lastName}
+                        </h2>
+                     }
+                     transition="rotate-right"
                   >
-                     Профиль
-                  </Menu.Item>
-                  <Menu.Item
-                     onClick={() => {
-                        mutate(null)
-                     }}
-                     className="hover:bg-blue-100"
-                  >
-                     Выйти
-                  </Menu.Item>
-               </Menu>
-            ) : (
-               <>
-                  <Button onClick={() => navigate('/login')}>Вход</Button>
-                  <Button onClick={() => navigate('/signup')} variant="outline">
-                     Регистрация
-                  </Button>
-               </>
-            )}
-         </div>
-      </div>
+                     <Menu.Item
+                        onClick={() => navigate('/profile')}
+                        className="hover:bg-blue-100"
+                     >
+                        Профиль
+                     </Menu.Item>
+                     <Menu.Item
+                        onClick={() => {
+                           mutate(null)
+                        }}
+                        className="hover:bg-blue-100"
+                     >
+                        Выйти
+                     </Menu.Item>
+                  </Menu>
+               ) : (
+                  <>
+                     <Button onClick={() => navigate('/login')}>Вход</Button>
+                     <Button
+                        onClick={() => navigate('/signup')}
+                        variant="outline"
+                     >
+                        Регистрация
+                     </Button>
+                  </>
+               )}
+            </div>
+         </nav>
+         <Modal onClose={onClose} opened={isOpen} title={<h2>Поиск</h2>}>
+            <Search onClose={onClose} />
+         </Modal>
+      </>
    )
 }
 
