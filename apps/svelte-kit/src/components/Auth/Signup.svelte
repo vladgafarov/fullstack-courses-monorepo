851c2506d76721d709e8f8209429b5d919a478f9
Vlad <vlad.gafarov02@gmail.com>
2022-03-03 21:59:48 +0500

    signup field validation

--- apps/svelte-kit/src/components/Auth/Signup.svelte
+++ apps/svelte-kit/src/components/Auth/Signup.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
    import {
-      LogInDocument,
       SignUpDocument,
-      type LogInMutation,
-      type LogInMutationVariables,
       type SignUpMutation,
       type SignUpMutationVariables
    } from '$api/generated'
    import { goto } from '$app/navigation'
+   import { validateForm } from '$lib/utils'
    import { mutation, operationStore } from '@urql/svelte'
+   import * as yup from 'yup'
+
+   const schema = yup.object().shape({
+      name: yup.string().required('Обязательно'),
+      lastName: yup.string().required('Обязательно'),
+      email: yup.string().required('Обязательно').email('Неверный email'),
+      password: yup.string().required('Обязательно')
+   })
 
    const signup = mutation(operationStore<SignUpMutation, SignUpMutationVariables>(SignUpDocument))
 
@@ -17,26 +23,36 @@
    let email: string = ''
    let password: string = ''
 
-   let error: string = ''
+   let signupError: string = ''
+   let formErrors: Record<string, unknown> | undefined
    let loading: boolean = false
 
    const handleSubmit = async () => {
-      loading = true
+      console.log('wtf')
 
-      signup({
-         name,
-         lastName,
-         email,
-         password
-      }).then((res) => {
-         loading = false
-         if (res.error) {
-            error = res.error.graphQLErrors[0].message
-         } else {
-            error = ''
-            goto('/')
-         }
-      })
+      signupError = ''
+      loading = true
+      const values = { name, lastName, email, password }
+
+      formErrors = await validateForm(schema, values)
+
+      if (!formErrors) {
+         signup({
+            name,
+            lastName,
+            email,
+            password
+         }).then((res) => {
+            if (res.error) {
+               signupError = res.error.graphQLErrors[0].message
+            } else {
+               signupError = ''
+               goto('/')
+            }
+         })
+      }
+
+      loading = false
    }
 </script>
 
@@ -47,22 +63,34 @@
       <label>
          <span>Имя</span>
          <input name="name" bind:value={name} />
+         {#if formErrors?.name}
+            <p class="error">{formErrors.name}</p>
+         {/if}
       </label>
       <label>
          <span>Фамилия</span>
          <input name="lastName" bind:value={lastName} />
+         {#if formErrors?.lastName}
+            <p class="error">{formErrors.lastName}</p>
+         {/if}
       </label>
       <label>
          <span>Почта</span>
-         <input name="email" bind:value={email} />
+         <input name="email" type="email" bind:value={email} />
+         {#if formErrors?.email}
+            <p class="error">{formErrors.email}</p>
+         {/if}
       </label>
       <label>
          <span>Пароль</span>
          <input name="password" type="password" bind:value={password} />
+         {#if formErrors?.password}
+            <p class="error">{formErrors.password}</p>
+         {/if}
       </label>
 
-      {#if error}
-         <p class:error>{error}</p>
+      {#if signupError}
+         <p class:error={signupError}>{signupError}</p>
       {/if}
 
       <button class="btn btn-primary" disabled={loading}>Зарегистрироваться</button>
