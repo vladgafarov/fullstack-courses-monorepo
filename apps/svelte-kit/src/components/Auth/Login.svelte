ace399aa23a6705b431d61015ea0a270d71ffdd1
Vlad <vlad.gafarov02@gmail.com>
2022-03-03 21:51:29 +0500

    check fields login

--- apps/svelte-kit/src/components/Auth/Login.svelte
+++ apps/svelte-kit/src/components/Auth/Login.svelte
@@ -1,30 +1,46 @@
 <script lang="ts">
    import { LogInDocument, type LogInMutation, type LogInMutationVariables } from '$api/generated'
    import { goto } from '$app/navigation'
+   import { validateForm } from '$lib/utils'
    import { mutation, operationStore } from '@urql/svelte'
+   import * as yup from 'yup'
+
+   const schema = yup.object().shape({
+      email: yup.string().required('Обязательно').email('Неверный email'),
+      password: yup.string().required('Обязательно')
+   })
 
    const login = mutation(operationStore<LogInMutation, LogInMutationVariables>(LogInDocument))
 
    let email: string = 'jhon@test.com'
-   let password: string = '1234567'
+   let password: string = '123456'
 
-   let error: string = ''
+   let loginError: string = ''
+   let formErrors: Record<string, unknown> | undefined
    let loading: boolean = false
 
    const handleSubmit = async () => {
+      loginError = ''
+      const values = { email, password }
+
       loading = true
-      login({
-         email,
-         password
-      }).then((res) => {
-         loading = false
-         if (res.error) {
-            error = res.error.graphQLErrors[0].message
-         } else {
-            error = ''
-            goto('/')
-         }
-      })
+      formErrors = await validateForm<{ email: string; password: string }>(schema, values)
+
+      if (!formErrors) {
+         login({
+            email,
+            password
+         }).then((res) => {
+            if (res.error) {
+               loginError = res.error.graphQLErrors[0].message
+            } else {
+               loginError = ''
+               goto('/')
+            }
+         })
+      }
+
+      loading = false
    }
 </script>
 
@@ -34,15 +50,21 @@
    <form on:submit|preventDefault={handleSubmit} class="flex flex-col space-y-3">
       <label>
          <span>Почта</span>
-         <input name="email" bind:value={email} />
+         <input name="email" type="email" bind:value={email} />
+         {#if formErrors?.email}
+            <p class="error">{formErrors.email}</p>
+         {/if}
       </label>
       <label>
          <span>Пароль</span>
          <input name="password" type="password" bind:value={password} />
+         {#if formErrors?.password}
+            <p class="error">{formErrors.password}</p>
+         {/if}
       </label>
 
-      {#if error}
-         <p class:error>{error}</p>
+      {#if loginError}
+         <p class:error={loginError}>{loginError}</p>
       {/if}
 
       <button class="btn btn-primary" disabled={loading}>Войти</button>
--- apps/svelte-kit/src/components/Auth/Signup.svelte
+++ apps/svelte-kit/src/components/Auth/Signup.svelte
@@ -1,5 +1,72 @@
+<script lang="ts">
+   import {
+      LogInDocument,
+      SignUpDocument,
+      type LogInMutation,
+      type LogInMutationVariables,
+      type SignUpMutation,
+      type SignUpMutationVariables
+   } from '$api/generated'
+   import { goto } from '$app/navigation'
+   import { mutation, operationStore } from '@urql/svelte'
+
+   const signup = mutation(operationStore<SignUpMutation, SignUpMutationVariables>(SignUpDocument))
+
+   let name: string = ''
+   let lastName: string = ''
+   let email: string = ''
+   let password: string = ''
+
+   let error: string = ''
+   let loading: boolean = false
+
+   const handleSubmit = async () => {
+      loading = true
+
+      signup({
+         name,
+         lastName,
+         email,
+         password
+      }).then((res) => {
+         loading = false
+         if (res.error) {
+            error = res.error.graphQLErrors[0].message
+         } else {
+            error = ''
+            goto('/')
+         }
+      })
+   }
+</script>
+
 <div class="max-w-md mx-auto my-6 bg-slate-200 p-4 lg:p-6 rounded-lg shadow-md">
    <h1 class="underline decoration-blue-500">Регистрация</h1>
+
+   <form on:submit|preventDefault={handleSubmit} class="flex flex-col space-y-3">
+      <label>
+         <span>Имя</span>
+         <input name="name" bind:value={name} />
+      </label>
+      <label>
+         <span>Фамилия</span>
+         <input name="lastName" bind:value={lastName} />
+      </label>
+      <label>
+         <span>Почта</span>
+         <input name="email" bind:value={email} />
+      </label>
+      <label>
+         <span>Пароль</span>
+         <input name="password" type="password" bind:value={password} />
+      </label>
+
+      {#if error}
+         <p class:error>{error}</p>
+      {/if}
+
+      <button class="btn btn-primary" disabled={loading}>Зарегистрироваться</button>
+   </form>
 
    <p class="text-center underline pt-3">
       <a href="/login">Вход</a>
